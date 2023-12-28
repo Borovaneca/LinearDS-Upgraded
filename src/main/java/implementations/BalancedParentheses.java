6daeabf3bf0a573c22e6455ba11af4117c2cb9a5
Borovaneca <petyoveselinov@gmail.com>
2023-12-29 00:37:44 +0200

    Updated BalancedParentheses ...

--- src/main/java/implementations/BalancedParentheses.java
+++ src/main/java/implementations/BalancedParentheses.java
@@ -2,6 +2,10 @@ package implementations;
 
 import interfaces.Solvable;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Stack;
+
 public class BalancedParentheses implements Solvable {
     private String parentheses;
 
@@ -11,6 +15,30 @@ public class BalancedParentheses implements Solvable {
 
     @Override
     public Boolean solve() {
-        return null;
+        if (this.parentheses == null || this.parentheses.isEmpty()) {
+            return true;
+        }
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < this.parentheses.length(); i++) {
+            char currentChar = this.parentheses.charAt(i);
+
+            if (currentChar == '(' || currentChar == '[' || currentChar == '{') {
+                stack.push(currentChar);
+            } else if (currentChar == ')' || currentChar == ']' || currentChar == '}') {
+                if (stack.isEmpty() || !areParenthesesMatching(stack.pop(), currentChar)) {
+                    return false;
+                }
+            }
+
+        }
+        return stack.isEmpty();
+    }
+
+    private boolean areParenthesesMatching(Character opening, char closing) {
+        return (opening == '(' && closing == ')') ||
+                (opening == '[' && closing == ']') ||
+                (opening == '{' && closing == '}');
     }
 }
